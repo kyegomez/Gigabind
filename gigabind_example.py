e4c9030fb21bfe5e352110ee5608a9b913046bc1
Zack <zack@zackbradshaw.com>
2023-12-11 17:29:59 -0800

    feat: update gigabind example

--- gigabind_example.py
+++ gigabind_example.py
@@ -13,18 +13,18 @@ input_data = {"text": ["Hello, world!"]}
 response = requests.post(
     server_url, headers=headers, data=json.dumps({"input_data": input_data})
 )
-print(response.json())
+print("Text modality response:", response.json())
 
 # Example for audio modality
 # Assuming audio_file is a file object opened in binary mode
 with open("path_to_your_audio_file", "rb") as audio_file:
     files = {"audio": audio_file}
     response = requests.post(server_url, headers=headers, files=files)
-    print(response.json())
+    print("Audio modality response:", response.json())
 
 # Example for vision modality
 # Assuming image_file is a file object opened in binary mode
 with open("path_to_your_image_file", "rb") as image_file:
     files = {"vision": image_file}
     response = requests.post(server_url, headers=headers, files=files)
-    print(response.json())
+    print("Vision modality response:", response.json())
