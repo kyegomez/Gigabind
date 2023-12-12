a4dda8588c5f9a27cefbed6e75acc094a1f32307
Zack <zack@zackbradshaw.com>
2023-12-11 16:52:12 -0800

    update: update auth

--- gigabind_example.py
+++ gigabind_example.py
@@ -1,11 +1,32 @@
-import modal
-from modal import Client
+import requests
+import json
 
-server_url = "http://localhost:5000"  # replace with your server URL
-client_type = "example_type"  # replace with your client type
-credentials = {"username": "example", "password": "example"}  # replace with your credentials
+# replace with your server URL
+server_url = "http://localhost:5000/process"  
+# replace with your API key
+api_key = "your_api_key"  
 
-client = Client(server_url=,client_type=,credentials=)
-result = client.call("gigabind", {"input_data": {"text": ["Hello, world!"]}})
-print(result)
+headers = {
+    "Authorization": f"Bearer {api_key}",
+    "Content-Type": "application/json"
+}
+
+# Example for text modality
+input_data = {"text": ["Hello, world!"]}
+response = requests.post(server_url, headers=headers, data=json.dumps({"input_data": input_data}))
+print(response.json())
+
+# Example for audio modality
+# Assuming audio_file is a file object opened in binary mode
+with open("path_to_your_audio_file", "rb") as audio_file:
+    files = {"audio": audio_file}
+    response = requests.post(server_url, headers=headers, files=files)
+    print(response.json())
+
+# Example for vision modality
+# Assuming image_file is a file object opened in binary mode
+with open("path_to_your_image_file", "rb") as image_file:
+    files = {"vision": image_file}
+    response = requests.post(server_url, headers=headers, files=files)
+    print(response.json())
 
