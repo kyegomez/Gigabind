8ff1490896af2fd08cf4bb3c86f30e85092fdae7
Kye <kye@apacmediasolutions.com>
2023-12-16 21:58:50 -0500

    curl update

--- curl.sh
+++ curl.sh
@@ -3,7 +3,5 @@ curl -X 'POST' \
   -H 'accept: application/json' \
   -H 'Content-Type: application/json' \
   -d '{
-  "text_list": ["bird", "car", "dog"],
-  "image_paths": ["path/to/bird_image.jpg", "path/to/car_image.jpg", "path/to/dog_image.jpg"],
-  "audio_paths": ["path/to/bird_audio.wav", "path/to/car_audio.wav", "path/to/dog_audio.wav"]
+  "text_list": ["bird", "car", "dog"]
 }'
