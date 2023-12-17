60ed5389f0783273e2ab76f0a6a38756fd5ed973
Kye <kye@apacmediasolutions.com>
2023-12-17 03:50:59 -0500

    [CLEANUP]

--- gigabind/main.py
+++ gigabind/main.py
@@ -6,6 +6,8 @@ import logging
 from gigabind.models import imagebind_model
 from gigabind.models.imagebind_model import ModalityType, load_module
 from typing import Callable
+from types import SimpleNamespace
+
 
 # Setup Logging
 logging.basicConfig(level=logging.INFO, force=True)
@@ -49,11 +51,11 @@ class Gigabind(nn.Module):
             lora_factor = 1
 
         # Instantiate model
-        model = imagebind_model.imagebind_huge(pretrained=True)
+        self.model = imagebind_model.imagebind_huge(pretrained=True)
         if lora:
-            model.modality_trunks.update(
+            self.model.modality_trunks.update(
                 lora.apply_lora_modality_trunks(
-                    model.modality_trunks,
+                    self.model.modality_trunks,
                     rank=4,
                     # layer_idxs={ModalityType.TEXT: [0, 1, 2, 3, 4, 5, 6, 7, 8],
                     #             ModalityType.VISION: [0, 1, 2, 3, 4, 5, 6, 7, 8]},
@@ -63,7 +65,7 @@ class Gigabind(nn.Module):
 
             # Load lora params if found
             lora.load_lora_modality_trunks(
-                model.modality_trunks,
+                self.model.modality_trunks,
                 checkpoint_dir=".checkpoints/lora/550_epochs_lora",
                 postfix="_dreambooth_last",
             )
@@ -71,13 +73,13 @@ class Gigabind(nn.Module):
             if load_head_post_proc_finetuned:
                 # Load postprocessors & heads
                 load_module(
-                    model.modality_postprocessors,
+                    self.model.modality_postprocessors,
                     module_name="postprocessors",
                     checkpoint_dir=".checkpoints/lora/550_epochs_lora",
                     postfix="_dreambooth_last",
                 )
                 load_module(
-                    model.modality_heads,
+                    self.model.modality_heads,
                     module_name="heads",
                     checkpoint_dir=".checkpoints/lora/550_epochs_lora",
                     postfix="_dreambooth_last",
@@ -85,7 +87,7 @@ class Gigabind(nn.Module):
         elif linear_probing:
             # Load heads
             load_module(
-                model.modality_heads,
+                self.model.modality_heads,
                 module_name="heads",
                 checkpoint_dir="./.checkpoints/lora/500_epochs_lp",
                 postfix="_dreambooth_last",
@@ -113,12 +115,12 @@ class Gigabind(nn.Module):
             if text is not None:
                 inputs["text"] = text
             if img is not None:
-                inputs["img"] = img
+                inputs["vision"] = img
             if audio is not None:
                 inputs["audio"] = audio
 
-            # Compute the embeddings
-            embeddings = self.model(inputs)
+            with torch.no_grad():
+                embeddings = self.model(inputs)
 
             # Prepare the response
             response = {
@@ -127,10 +129,35 @@ class Gigabind(nn.Module):
                     inputs.keys()
                 ),  # The types of modalities in the inputs
                 "model_name": "gigabind-huge",  # The name of the model
-                # Add any other information you want to include in the response
             }
 
             return response
         except Exception as e:
             logging.error(e)
             return e
+
+    def print_modalities_available(self):
+        """Prints the modalities available for the model"""
+        ModalityType = SimpleNamespace(
+            VISION="vision",
+            TEXT="text",
+            AUDIO="audio",
+            THERMAL="thermal",
+            DEPTH="depth",
+            IMU="imu",
+        )
+        print("Modalities Available:")
+        print(ModalityType.VISION)
+        print(ModalityType.TEXT)
+        print(ModalityType.AUDIO)
+        print(ModalityType.THERMAL)
+        print(ModalityType.DEPTH)
+        print(ModalityType.IMU)
+
+    def print_model(self):
+        """Prints the model"""
+        print(self.model)
+
+    def print_model_parameters(self):
+        """prints the model parameters"""
+        print(self.model.parameters())
