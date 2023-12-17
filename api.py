822138e143b36cc430c7333227409ef9cb210d6a
Kye <kye@apacmediasolutions.com>
2023-12-16 21:49:49 -0500

    [LORA]

--- api.py
+++ api.py
@@ -23,11 +23,11 @@ load_head_post_proc_finetuned = True
 
 # Ensure configuration is valid
 assert not (linear_probing and lora), (
-    "Linear probing is a subset of LoRA training procedure for ImageBind. "
+    "Linear probing is a subset of lora training procedure for ImageBind. "
     "Cannot set both linear_probing=True and lora=True. "
 )
 
-# Factor adjustment based on LoRA settings
+# Factor adjustment based on lora settings
 if lora and not load_head_post_proc_finetuned:
     lora_factor = 12 / 0.07
 else:
@@ -37,7 +37,7 @@ else:
 model = imagebind_model.imagebind_huge(pretrained=True)
 if lora:
     model.modality_trunks.update(
-        LoRA.apply_lora_modality_trunks(
+        lora.apply_lora_modality_trunks(
             model.modality_trunks,
             rank=4,
             # layer_idxs={ModalityType.TEXT: [0, 1, 2, 3, 4, 5, 6, 7, 8],
@@ -46,8 +46,8 @@ if lora:
         )
     )
 
-    # Load LoRA params if found
-    LoRA.load_lora_modality_trunks(
+    # Load lora params if found
+    lora.load_lora_modality_trunks(
         model.modality_trunks,
         checkpoint_dir=".checkpoints/lora/550_epochs_lora",
         postfix="_dreambooth_last",
