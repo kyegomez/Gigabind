ab0fd77f87e9902c2749e42753f3d8f6debd9370
Kye <kye@apacmediasolutions.com>
2023-12-16 22:02:40 -0500

    assets

--- curl.sh
+++ curl.sh
@@ -3,5 +3,7 @@ curl -X 'POST' \
   -H 'accept: application/json' \
   -H 'Content-Type: application/json' \
   -d '{
-  "text_list": ["bird", "car", "dog"]
+  "text_list": ["bird", "car", "dog"],
+  "image_paths": [".assets/bird_image.jpg", ".assets/car_image.jpg", ".assets/dog_image.jpg"],
+  "audio_paths": [".assets/bird_audio.wav", ".assets/car_audio.wav", ".assets/dog_audio.wav"]
 }'
